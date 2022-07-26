6f61fb2f196b49a0354c82aced9be28b856d2e89
dylanberry <dylan@dylanberry.com>
2022-07-26 16:09:00 -0400

    debugging

--- export-report.sh
+++ export-report.sh
@@ -76,6 +76,7 @@ curl -X POST "$baseUri/groups/$sourceGroupId/reports/$exportReportId/Rebind" \
 
 sourceReportFilePath="$PWD/$SourceReportName.zip"
 echo "Exporting report $exportReportName to $sourceReportFilePath"
+echo "$baseUri/groups/$sourceGroupId/reports/$exportReportId/Export?preferClientRouting=true"
 curl "$baseUri/groups/$sourceGroupId/reports/$exportReportId/Export?preferClientRouting=true" \
   -H "Authorization: Bearer $accessToken" \
   -o $sourceReportFilePath
